1438f7c54560159cb84f8468169dd49f6885521a
Shrey27a <sharmashreya65214@gmail.com>
2025-07-07 12:08:48 +0530

    First Commit

---
+++ basics/ternary.js
@@ -0,0 +1,12 @@
+let a=((10>5)?(10):(7));
+console.log(a);
+
+let b = (10 < 5) ? (10) : (7)
+console.log(b);
+
+let c=((true)?(2+3):(2-3));
+console.log(c);
+
+let d =10;
+let e =((3<1)?(2-3):(++d));
+console.log(e);
